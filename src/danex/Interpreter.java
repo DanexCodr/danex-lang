e0381afcf780a79a7c108a8afd1c80fffd8e0bdb
Danison Nuñez <danisonnunez001@gmail.com>
2025-06-17 01:04:07 +0800

    Update Interpreter.java

--- src/danex/Interpreter.java
+++ src/danex/Interpreter.java
@@ -1,254 +1,354 @@
 package danex;
 
-import danex.ast.*;
-import java.util.*;
+import danex.ast.;
+import java.util.;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>, Decl.Visitor<Void> {
+private Environment globals = new Environment();
+private Environment environment = globals;
 
-    private Environment globals = new Environment();
-    private Environment environment = globals;
-
-    public Environment getGlobals() {
-        return globals;
-    }
-
-    public void interpret(List<Object> statementsAndDecls) {
-        try {
-            for (Object obj : statementsAndDecls) {
-                if (obj instanceof Stmt) {
-                    execute((Stmt) obj);
-                } else if (obj instanceof Decl) {
-                    declare((Decl) obj);
-                }
-            }
-        } catch (RuntimeError error) {
-            System.err.println("[Runtime Error] " + error.getMessage());
-        }
-    }
-
-    private void execute(Stmt stmt) {
-        stmt.accept(this);
-    }
-
-    private void declare(Decl decl) {
-        decl.accept(this);
-    }
-
-    public void executeBlock(List<Object> body, Environment newEnv) {
-        Environment previous = this.environment;
-        try {
-            this.environment = newEnv;
-            for (Object obj : body) {
-                if (obj instanceof Stmt) {
-                    execute((Stmt) obj);
-                } else if (obj instanceof Decl) {
-                    declare((Decl) obj);
-                }
-            }
-        } finally {
-            this.environment = previous;
-        }
-    }
-
-    // --------- Stmt Visitors ---------
-
-    @Override
-    public Void visitExpressionStmt(ExpressionStmt stmt) {
-        evaluate(stmt.expression);
-        return null;
-    }
-
-    @Override
-    public Void visitVarStmt(VarStmt stmt) {
-        Object value = null;
-        if (stmt.initializer != null) {
-            value = evaluate(stmt.initializer);
-        }
-        environment.define(stmt.name.lexeme, value);
-        return null;
-    }
-
-    @Override
-    public Void visitAssignStmt(AssignStmt stmt) {
-        Object value = evaluate(stmt.value);
-        environment.assign(stmt.name, value);
-        return null;
-    }
-
-    @Override
-    public Void visitPrintStmt(PrintStmt stmt) {
-        Object value = evaluate(stmt.expression);
-        System.out.println(stringify(value));
-        return null;
-    }
-
-    @Override
-    public Void visitReturnStmt(ReturnStmt stmt) {
-        Object value = null;
-        if (stmt.value != null) value = evaluate(stmt.value);
-        throw new Return(value);
-    }
-
-    @Override
-    public Void visitBlockStmt(BlockStmt stmt) {
-        executeBlock(stmt.statements, new Environment(environment));
-        return null;
-    }
-
-    @Override
-    public Void visitIfStmt(IfStmt stmt) {
-        if (isTruthy(evaluate(stmt.condition))) {
-            execute(stmt.thenBranch);
-        } else if (stmt.elseBranch != null) {
-            execute(stmt.elseBranch);
-        }
-        return null;
-    }
-
-    @Override
-    public Void visitWhileStmt(WhileStmt stmt) {
-        while (isTruthy(evaluate(stmt.condition))) {
-            execute(stmt.body);
-        }
-        return null;
-    }
-
-    @Override
-    public Void visitExitStmt(ExitStmt stmt) {
-        System.exit(0);
-        return null;
-    }
-
-    // --------- Decl Visitors ---------
-
-    @Override
-    public Void visitClassDecl(ClassDecl decl) {
-        DanexClass klass = new DanexClass(decl.name.lexeme);
-        environment.define(decl.name.lexeme, klass);
-        return null;
-    }
-
-    @Override
-    public Void visitMethodDecl(MethodDecl decl) {
-        DanexFunction function = new DanexFunction(decl, environment);
-        environment.define(decl.name.lexeme, function);
-        return null;
-    }
-
-    @Override
-    public Void visitImportDecl(ImportDecl decl) {
-        // Placeholder: implement your import system if any
-        return null;
-    }
-
-    @Override
-    public Void visitResourceDecl(ResourceDecl decl) {
-        // Optional: implement if needed
-        return null;
-    }
-
-    // These are placeholders until you create actual AST classes for Annotation and Param
-
-    @Override public Void visitAnnotation(Annotation a) {
-        return null;
-    }
-
-    @Override public Void visitParam(Param p) {
-        return null;
-    }
-
-    // --------- Expression Visitors ---------
-
-    @Override
-    public Object visitLiteralExpr(LiteralExpr expr) {
-        return expr.value;
-    }
-
-    @Override
-    public Object visitVariableExpr(VariableExpr expr) {
-        return environment.get(expr.name);
-    }
-
-    @Override
-    public Object visitAssignExpr(AssignExpr expr) {
-        Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
-        return value;
-    }
-
-    @Override
-    public Object visitBinaryExpr(BinaryExpr expr) {
-        Object left = evaluate(expr.left);
-        Object right = evaluate(expr.right);
-
-        switch (expr.operator.type) {
-            case PLUS:
-                if (left instanceof Double && right instanceof Double) {
-                    return (double) left + (double) right;
-                }
-                if (left instanceof String || right instanceof String) {
-                    return stringify(left) + stringify(right);
-                }
-                throw new RuntimeError(expr.operator, "Operands must be numbers or strings.");
-            case MINUS:
-                return (double) left - (double) right;
-            case STAR:
-                return (double) left * (double) right;
-            case SLASH:
-                return (double) left / (double) right;
-            case GREATER:
-                return (double) left > (double) right;
-            case GREATER_EQUAL:
-                return (double) left >= (double) right;
-            case LESS:
-                return (double) left < (double) right;
-            case LESS_EQUAL:
-                return (double) left <= (double) right;
-            case EQUAL_EQUAL:
-                return Objects.equals(left, right);
-            case BANG_EQUAL:
-                return !Objects.equals(left, right);
-        }
-
-        return null;
-    }
-
-    @Override
-    public Object visitGroupingExpr(GroupingExpr expr) {
-        return evaluate(expr.expression);
-    }
-
-    @Override
-    public Object visitCallExpr(CallExpr expr) {
-        Object callee = evaluate(expr.callee);
-
-        List<Object> arguments = new ArrayList<>();
-        for (Expr arg : expr.arguments) {
-            arguments.add(evaluate(arg));
-        }
-
-        if (!(callee instanceof DanexCallable)) {
-            throw new RuntimeError(expr.paren, "Can only call functions and classes.");
-        }
-
-        DanexCallable function = (DanexCallable) callee;
-        return function.call(this, arguments);
-    }
-
-    // --------- Utilities ---------
-
-    private Object evaluate(Expr expr) {
-        return expr.accept(this);
-    }
-
-    private boolean isTruthy(Object obj) {
-        if (obj == null) return false;
-        if (obj instanceof Boolean) return (Boolean) obj;
-        return true;
-    }
-
-    private String stringify(Object obj) {
-        if (obj == null) return "nil";
-        return obj.toString();
-    }
+public Interpreter() {
+setupBuiltins();
 }
+
+private void setupBuiltins() {  
+   globals.define("print", new DanexCallable() {  
+@Override  
+public Object call(Interpreter interpreter, List<Object> args) {  
+    for (Object arg : args) {  
+        System.out.println(arg);  
+    }  
+    return null;  
+}  
+
+@Override  
+public String toString() {  
+    return "<builtin fn print>";  
+}
+
+});
+}
+
+public void interpret(List<Stmt> statements) {  
+    try {  
+        for (Stmt stmt : statements) execute(stmt);  
+    } catch (RuntimeError error) {  
+        System.err.println("Runtime error: " + error.getMessage());  
+    }  
+}  
+
+public void interpretDecls(List<Decl> decls) {  
+    try {  
+        for (Decl decl : decls) decl.accept(this);  
+    } catch (RuntimeError error) {  
+        System.err.println("Runtime error in declaration: " + error.getMessage());  
+    }  
+}
+
+public void executeInEnvironment(Stmt body, Environment newEnv) {
+Environment previous = this.environment;
+try {
+this.environment = newEnv;
+execute(body);
+} finally {
+this.environment = previous;
+}
+}
+
+private void execute(Stmt stmt) { stmt.accept(this); }  
+private Object evaluate(Expr expr) { return expr.accept(this); }  
+
+private boolean isTruthy(Object obj) {  
+    if (obj == null) return false;  
+    if (obj instanceof Boolean) return (Boolean) obj;  
+    return true;  
+}  
+
+private boolean isEqual(Object a, Object b) {  
+    if (a == null && b == null) return true;  
+    if (a == null) return false;  
+    return a.equals(b);  
+}  
+
+private void checkNumberOperand(String op, Object operand) {  
+    if (!(operand instanceof Double)) throw new RuntimeError("Operand must be a number for '" + op + "'.");  
+}  
+
+private double toNumber(Object obj) { return (Double) obj; }  
+private String toStr(Object obj) { return obj == null ? "null" : obj.toString(); }
+
+@Override
+public Void visitParam(Param param) {
+return null;
+}
+
+@Override
+public Void visitAnnotation(Annotation annotation) {
+// No-op, or implement annotation behavior here
+return null;
+}
+@Override
+public Object visitLiteralExpr(LiteralExpr literalExpr) {
+return literalExpr.value;
+}
+
+@Override  
+public Object visitGroupingExpr(GroupingExpr groupingExpr) {  
+    return evaluate(groupingExpr.expression);  
+}  
+
+@Override  
+public Object visitVariableExpr(VariableExpr variableExpr) {  
+    return environment.get(variableExpr.name);  
+}  
+
+@Override  
+public Object visitAssignExpr(AssignExpr assignExpr) {  
+    Object value = evaluate(assignExpr.value);  
+    environment.assign(assignExpr.name, value);  
+    return value;  
+}  
+
+@Override  
+public Object visitBinaryExpr(BinaryExpr binaryExpr) {  
+    Object left = evaluate(binaryExpr.left);  
+    Object right = evaluate(binaryExpr.right);  
+    String op = binaryExpr.operator;  
+    switch (op) {  
+        case "+":  
+            if (left instanceof Double && right instanceof Double) return toNumber(left) + toNumber(right);  
+            if (left instanceof String || right instanceof String) return toStr(left) + toStr(right);  
+            throw new RuntimeError("Operands must be two numbers or one must be string for '+'.");  
+        case "-":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            return toNumber(left) - toNumber(right);  
+        case "*":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            return toNumber(left) * toNumber(right);  
+        case "/":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            if (toNumber(right) == 0) throw new RuntimeError("Division by zero.");  
+            return toNumber(left) / toNumber(right);  
+        case "%":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            return toNumber(left) % toNumber(right);  
+        case "<":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            return toNumber(left) < toNumber(right);  
+        case ">":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            return toNumber(left) > toNumber(right);  
+        case "<=":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            return toNumber(left) <= toNumber(right);  
+        case ">=":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            return toNumber(left) >= toNumber(right);  
+        case "==":  
+            return isEqual(left, right);  
+        case "!=":  
+            return !isEqual(left, right);  
+        case "&&":  
+            return isTruthy(left) && isTruthy(right);  
+        case "||":  
+            return isTruthy(left) || isTruthy(right);  
+        case "??":  
+            return left != null ? left : right;  
+        case "<=>":  
+            checkNumberOperand(op, left); checkNumberOperand(op, right);  
+            double l = toNumber(left), r = toNumber(right);  
+            return l < r ? -1.0 : (l > r ? 1.0 : 0.0);  
+        default:  
+            throw new RuntimeError("Unknown operator '" + op + "'.");  
+    }  
+}  
+
+@Override  
+public Object visitUnaryExpr(UnaryExpr unaryExpr) {  
+    Object rightVal = evaluate(unaryExpr.right);  
+    String opu = unaryExpr.operator;  
+    switch (opu) {  
+        case "-":  
+            checkNumberOperand(opu, rightVal);  
+            return -toNumber(rightVal);  
+        case "!":  
+            return !isTruthy(rightVal);  
+        default:  
+            throw new RuntimeError("Unknown unary operator '" + opu + "'.");  
+    }  
+}  
+
+@Override
+
+public Object visitCallExpr(CallExpr callExpr) {
+Object callee = evaluate(callExpr.callee);
+List<Object> args = new ArrayList<>();
+for (Expr arg : callExpr.arguments) args.add(evaluate(arg));
+
+if (!(callee instanceof DanexCallable)) {  
+    throw new RuntimeError("Can only call functions and classes.");  
+}  
+
+DanexCallable function = (DanexCallable) callee;  
+return function.call(this, args);
+
+}
+
+@Override  
+public Object visitLambdaExpr(LambdaExpr lambdaExpr) {  
+    throw new RuntimeError("Lambdas not supported yet.");  
+}  
+
+@Override  
+public Object visitNullCoalesceExpr(NullCoalesceExpr nullCoalesceExpr) {  
+    Object lVal = evaluate(nullCoalesceExpr.left);  
+    if (lVal != null) return lVal;  
+    return evaluate(nullCoalesceExpr.right);  
+}  
+
+@Override  
+public Object visitComparatorExpr(ComparatorExpr comparatorExpr) {  
+    Object lc = evaluate(comparatorExpr.left);  
+    Object rc = evaluate(comparatorExpr.right);  
+    if (!(lc instanceof Double) || !(rc instanceof Double)) throw new RuntimeError("Operands must be numbers for '<=>'.");  
+    double ld = toNumber(lc), rd = toNumber(rc);  
+    return ld < rd ? -1.0 : (ld > rd ? 1.0 : 0.0);  
+}  
+
+@Override  
+public Object visitAwaitExpr(AwaitExpr awaitExpr) {  
+    throw new RuntimeError("Await not supported yet.");  
+}  
+
+@Override  
+public Object visitDoExpr(DoExpr doExpr) {  
+    throw new RuntimeError("Do-expression not supported yet.");  
+}  
+
+@Override  
+public Object visitTryExpr(TryExpr tryExpr) {  
+    // Evaluate try-expression: TODO implement semantics  
+    throw new RuntimeError("Try-expression not supported yet.");  
+}
+
+@Override
+public Void visitAssignStmt(AssignStmt stmt) {
+Object value = evaluate(stmt.value);
+
+if (!(stmt.target instanceof VariableExpr)) {  
+    throw new RuntimeError("Invalid assignment target.");  
+}  
+
+String varName = ((VariableExpr) stmt.target).name;  
+environment.assign(varName, value);  
+return null;
+
+}
+
+@Override  
+public Void visitExprStmt(ExprStmt exprStmt) {  
+    evaluate(exprStmt.expression);  
+    return null;  
+}  
+
+@Override  
+public Void visitBlockStmt(BlockStmt blockStmt) {  
+    Environment previous = environment;  
+    environment = new Environment(previous);  
+    for (Stmt s : blockStmt.statements) execute(s);  
+    environment = previous;  
+    return null;  
+}  
+
+@Override  
+public Void visitIfStmt(IfStmt ifStmt) {  
+    if (isTruthy(evaluate(ifStmt.condition))) {  
+        execute(ifStmt.thenBranch);  
+    } else if (ifStmt.elseBranch != null) {  
+        execute(ifStmt.elseBranch);  
+    }  
+    return null;  
+}  
+
+@Override  
+public Void visitWhileStmt(WhileStmt whileStmt) {  
+    while (isTruthy(evaluate(whileStmt.condition))) {  
+        execute(whileStmt.body);  
+    }  
+    return null;  
+}  
+
+@Override  
+public Void visitDoWhileStmt(DoWhileStmt doWhileStmt) {  
+    do {  
+        execute(doWhileStmt.body);  
+    } while (isTruthy(evaluate(doWhileStmt.condition)));  
+    return null;  
+}  
+
+@Override  
+public Void visitForStmt(ForStmt forStmt) {  
+    if (forStmt.init != null) execute(forStmt.init);  
+    while (true) {  
+        if (forStmt.condition != null && !isTruthy(evaluate(forStmt.condition))) break;  
+        execute(forStmt.body);  
+        if (forStmt.update != null) evaluate(forStmt.update);  
+    }  
+    return null;  
+}  
+
+@Override  
+public Void visitThrowStmt(ThrowStmt throwStmt) {  
+    Object ex = evaluate(throwStmt.exception);  
+    throw new RuntimeError(toStr(ex));  
+}  
+
+@Override  
+public Void visitExitStmt(ExitStmt exitStmt) {  
+    System.exit(0);  
+    return null;  
+}  
+
+@Override  
+public Void visitTryStmt(TryStmt tryStmt) {  
+    try {  
+        for (Stmt s : tryStmt.tryBlock) execute(s);  
+    } catch (RuntimeError e) {  
+        // TODO: handle catch: type = " + tryStmt.catchType + ", name = " + tryStmt.catchName + "  
+        throw e;  
+    } finally {  
+        for (Stmt s : tryStmt.finallyBlock) execute(s);  
+    }  
+    return null;  
+}  
+
+@Override  
+public Void visitClassDecl(ClassDecl classDecl) {  
+    // TODO: implement class declaration: name = " + classDecl.name + "  
+    throw new RuntimeError("Class declarations not implemented yet.");  
+}
+
+@Override
+public Void visitMethodDecl(MethodDecl methodDecl) {
+DanexFunction function = new DanexFunction(methodDecl, environment);
+environment.define(methodDecl.name, function); // 🛠 Correct binding!
+return null;
+}
+
+@Override  
+public Void visitImportDecl(ImportDecl importDecl) {  
+    // 'use' / import: no-op or record module as needed  
+    return null;  
+}
+
+@Override
+public Void visitResourceDecl(ResourceDecl decl) {
+// No-op for now, unless you want resources to register something
+return null;
+}
+
+public Environment getGlobals() {
+return globals;
+}
+}
+
