bbf3b2f3cac1e2c365a5b812f402d14dcd183a08
Danison Nuñez <danisonnunez001@gmail.com>
2025-06-16 20:42:33 +0800

    Update RuntimeError.java

--- src/danex/RuntimeError.java
+++ src/danex/RuntimeError.java
@@ -1,6 +1,6 @@
 package danex;
 
-public class RuntimeError extends Exception {
+public class RuntimeError extends RuntimeException {
     public RuntimeError(String message) {
         super(message);
     }
